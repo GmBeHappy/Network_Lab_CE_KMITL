0a899cfd72987722a90357f3ee3c80011b794013
GmBeHappy <gmplk@hotmail.com>
2021-07-18 05:18:02 +0700

    edit path

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + 'public'));
 
 app.use(express.static(__dirname + '/assets'));
-app.use(express.static(__dirname + '/views'));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
