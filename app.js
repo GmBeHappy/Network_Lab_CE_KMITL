87610352c46ab7087e8682ff0b8c89fe113d0b41
GmBeHappy <gmplk@hotmail.com>
2021-07-18 04:57:39 +0700

    add services and contact

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const memberRouter = require('./routes/member');
+const contactRouter = require('./routes/contact');
+const servicesRouter = require('./routes/services');
 
 const app = express();
 
@@ -25,21 +27,23 @@ app.use(express.static(__dirname + '/assets'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/member', memberRouter);
+app.use('/contact', contactRouter);
+app.use('/services', servicesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    // res.render('error');
 });
 
 module.exports = app;
